59b8be099579d98485d82b72a78a3b186eafbb21
frvarty <62083536+frivarty@users.noreply.github.com>
2023-09-03 20:41:43 +0200

    Add files via upload

---
+++ client.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+<h1>
+ Client   
+</h1>
+</head>
+<body>
+<header>
+    client information drop down.
+</header>
+
+</body>
+
+</html>
---
+++ for.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+This will be another page that i will 
+</head>
+<body>
+    
+<footer>
+    Developed by frivarty, All rights reserved.
+</footer>
+</body>
+
+</html>
