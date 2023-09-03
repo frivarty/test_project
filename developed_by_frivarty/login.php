60bae13c44d91f94eaa91f156da3f3032831f599
frvarty <62083536+frivarty@users.noreply.github.com>
2023-09-03 20:28:15 +0200

    Add files via upload

---
+++ developed_by_frivarty/login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="css/utils.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/mobile.css">
+<title> LUANGWA TOWN COUNCIL</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+
+<div bg="img\ELEPHANT.jpg"  >
+<div class="container-fluid">
+<header class="head">
+    <p></p>
+    
+    <h2 align="center" >
+  LUANGWA TOWN COUNCIL
+</h2>
+    </header>
+    <div align="center">
+        <div align="center"> <img src="img\LOG.png " href="img" width="110px" alt="luangwa logo"></div>
+<div align="center">LTC SYSTEM</div>
+    <form action="GET" align="center">
+        <div>Email  : <a></a><a></a><input type="email" id="email" name="Email"> </div>
+        <p>
+
+        </p>
+        <div>Password :<input type="password" id="password" name="Password"> </div>
+        <p></p>
+            <div><button type="button" class="btn btn-outline-success">Login</button></div>
+            
+        
+    </form>
+</div> 
+<div align="center"><a href="reset_password.php">Forgot Password</a></div>
+</div>
+
+</div>
+
+
+    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
